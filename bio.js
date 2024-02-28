eda5776c59e557f892f7e0be1fc621a4bff42a1d
malaika75 <160205748+malaika75@users.noreply.github.com>
2024-02-27 22:13:47 -0800

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,27 @@
+var name1 = " malaika";
+//console.log(name1)
+name1 = "ruby";
+// console.log(name1)
+//bio data 
+/*let personname:string = "malaikazahid"
+let age:number = 21
+let ismarried:boolean = false
+console.log (personname)
+console.log(age)
+console.log(ismarried)*/
+var personname1 = "malaika";
+var age2 = 21;
+var isfollow = true;
+var sign = null;
+var pass = undefined;
+// console.log(personname1)
+// console.log (age2)
+// console.log (isfollow)
+// console.log (sign)
+// console.log (pass)
+var apple = "green";
+var banana = "yellow";
+var cherry = "purple";
+console.log(apple);
+console.log(banana);
+console.log(cherry);
